dd502b3abfc9a7973c79a0647898f450328ab719
Renan de Luca <Renan de Luca>
2023-10-09 14:29:27 -0300

    animations component updated

--- src/styles/Animations/animations.tsx
+++ src/styles/Animations/animations.tsx
@@ -3,12 +3,8 @@ import { animated } from "react-spring";
 import styled from "styled-components";
 
 export const Animated = styled(animated.div)`
-    @media (max-width:1024px) {
-        display: none;
-    }
+
 `
 export const Ref = styled.div`
-    @media (max-width:1024px) {
-        display: none;
-    }
+
 `    
